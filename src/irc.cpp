503b8111609f3c5a08a53287a7116c3f344faba2
Nick Calvi <nick@imacdanny.com>
2014-03-05 17:41:10 -0500

    changes

--- src/irc.cpp
+++ src/irc.cpp
@@ -294,14 +294,14 @@ void ThreadIRCSeed2(void* parg)
         }
         
         if (fTestNet) {
-            Send(hSocket, "JOIN #smallchangeTEST3\r");
-            Send(hSocket, "WHO #smallchangeTEST3\r");
+            Send(hSocket, "JOIN #AFCoinTEST3\r");
+            Send(hSocket, "WHO #AFCoinTEST3\r");
         } else {
-            // randomly join #smallchange00-#smallchange99
+            // randomly join #AFCoin00-#AFCoin99
             int channel_number = GetRandInt(100);
             channel_number = 0; // Litecoin: for now, just use one channel
-            Send(hSocket, strprintf("JOIN #smallchange%02d\r", channel_number).c_str());
-            Send(hSocket, strprintf("WHO #smallchange%02d\r", channel_number).c_str());
+            Send(hSocket, strprintf("JOIN #AFCoin%02d\r", channel_number).c_str());
+            Send(hSocket, strprintf("WHO #AFCoin%02d\r", channel_number).c_str());
         }
 
         int64 nStart = GetTime();
--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -34,9 +34,9 @@ QString BitcoinUnits::name(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("SMC");
-    case mBTC: return QString("mSMC");
-    case uBTC: return QString::fromUtf8("μSMC");
+    case BTC: return QString("AFC");
+    case mBTC: return QString("mAFC");
+    case uBTC: return QString::fromUtf8("μAFC");
     default: return QString("???");
     }
 }
@@ -45,9 +45,9 @@ QString BitcoinUnits::description(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("SmallChange");
-    case mBTC: return QString("Milli-SmallChange (1 / 1,000)");
-    case uBTC: return QString("Micro-SmallChange (1 / 1,000,000)");
+    case BTC: return QString("AFCoin");
+    case mBTC: return QString("Milli-AFCoin (1 / 1,000)");
+    case uBTC: return QString("Micro-AFCoin (1 / 1,000,000)");
     default: return QString("???");
     }
 }
